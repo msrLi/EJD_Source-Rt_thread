4f92f9ca656604c6ce3c73f13f2307584fd483f1
lihuibin <lihuibin705@163.com>
2019-08-12 18:03:42 +0800

    For system: 添加框架目录

---
+++ applications/printerServer/printerServer.c
@@ -0,0 +1,14 @@
+/**
+ * @file printerServer.c
+ * @brief  about some server
+ * @author  lenovo <xxx@lenovo.com>
+ * @version 1.0.0
+ * @date 2019-08-12
+ */
+
+/* Copyright(C) 2009-2019, Lenovo Inc.
+ * All right reserved
+ * 
+ */
+
+
